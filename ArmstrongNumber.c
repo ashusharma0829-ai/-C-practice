102ca5460460ecd90fb7c073af1a53cc2c6f780b
ashusharma0829-ai <ashusharma0829@gmail.com>
2026-03-31 23:23:24 +0530

    ArmstrongNumber.c

---
+++ ArmstrongNumber.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+    int n,temp,digit,sum=0;
+    printf("Enter n: ");
+    scanf("%d",&n);
+    temp = n;
+    while(temp!=0){
+        digit = temp%10;
+        sum = sum + digit*digit*digit;
+        temp = temp/10;
+    }
+    if(sum == n){
+        printf("Armstrong Number");
+    }else{
+        printf("Not Armstrong Number");
+    }
+    return 0;
+}
