811e58b1c6647828237f6d98b07307795e65c68c
ashusharma0829-ai <ashusharma0829@gmail.com>
2026-04-04 19:32:00 +0530

    Prime.c

--- Prime.c
+++ Prime.c
@@ -1,20 +1,24 @@
+//Program to Check Number is Prime or not
 #include<stdio.h>
 int main(){
-    int num,flag=0;
-    printf("enter num: ");
-    scanf("%d",&num);
-    if(num<=1){
-        printf("Not Prime");
+  int n,flag=0;
+  printf("Enter n: ");
+  //Taking input from the user
+  scanf("%d",&n);
+  if(n<=1){
+    printf("Not Prime");
+  }
+  for(int i=2;i<=n;i++){
+    if(n%i==0){
+        flag = 1;
+        break;
     }
-        for(int i=2;i<=num;i++){
-            if(num%i==0){
-                flag = 1;
-            }
-        }
-        if(flag==0){
-            printf("Not Prime");
-        }else{
-            printf("Prime");
-        }
+  }
+  //Condition to Check 
+  if(flag == 1){
+    printf("Prime");
+  }else{
+    printf("Not Prime");
+  }
     return 0;
 }
