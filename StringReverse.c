9ffb961ad8312ddb68f684d553342fab1dc05b4b
ashusharma0829-ai <ashusharma0829@gmail.com>
2026-04-04 19:51:29 +0530

    StringReverse.c

---
+++ StringReverse.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+
+// Program to reverse a string
+int main() {
+    char str[100];
+    int i, len;
+
+    printf("Enter a string: ");
+    scanf("%s", str);
+
+    len = strlen(str);
+
+    for(i = len - 1; i >= 0; i--) {
+        printf("%c", str[i]);
+    }
+
+    return 0;
+}
