99576e487027ab856940f7e0c7ebb7028197adf6
ashusharma0829-ai <ashusharma0829@gmail.com>
2026-04-04 19:30:35 +0530

    Update ArmstrongNumber.c

--- ArmstrongNumber.c
+++ ArmstrongNumber.c
@@ -1,18 +1,20 @@
+//Program to Check Armstrong number
 #include<stdio.h>
 int main(){
-    int n,temp,digit,sum=0;
-    printf("Enter n: ");
+    int n,temp,sum=0,digit;
+    printf("Enter Value of n: ");
+    //Taking input from the user
     scanf("%d",&n);
     temp = n;
     while(temp!=0){
         digit = temp%10;
-        sum = sum + digit*digit*digit;
+        sum = sum+digit*digit*digit;
         temp = temp/10;
     }
+    //Condition check Number is Armstrong or Not
     if(sum == n){
         printf("Armstrong Number");
     }else{
         printf("Not Armstrong Number");
     }
     return 0;
-}
